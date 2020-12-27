97c156714ad96e3103d88577b72722175076b494
Dmitry Ostroushko <dmitrylianne@gmail.com>
2020-12-27 13:40:48 +0300

    Update CreateTable.sql

--- CreateTable.sql
+++ CreateTable.sql
@@ -1,8 +1,9 @@
-CREATE TABLE PDCL
-(
-    Date date,
-    Customer int,
-    Deal int,
-    Currency varchar(3),
-    Sum float
+DROP TABLE IF EXISTS PDCL;  
+CREATE TABLE IF NOT EXISTS PDCL  
+(  
+    Date DATE,  
+    Customer INT,  
+    Deal INT,  
+    Currency VARCHAR(3),  
+    Sum FLOAT  
 );
